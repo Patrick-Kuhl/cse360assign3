42f8aa6eeff7526e21b4a1d6ca1cc11234037871
Patrick Kuhl <pkuhl95@gmail.com>
2016-02-21 16:41:58 -0700

    Second commit. Changed calculator class

--- cse360assign3/src/cse360assign3/CalculatorTest.java
+++ cse360assign3/src/cse360assign3/CalculatorTest.java
@@ -11,20 +11,6 @@ import org.junit.Test;
 
 public class CalculatorTest {
 
-	private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-	private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
-	
-	@Before
-	public void setUpStreams() {
-	    System.setOut(new PrintStream(outContent));
-	    System.setErr(new PrintStream(errContent));
-	}
-
-	@After
-	public void cleanUpStreams() {
-	    System.setOut(null);
-	    System.setErr(null);
-	}
 	@Test
 	public void testCalculator() {
 		Calculator calc = new Calculator();
@@ -33,48 +19,47 @@ public class CalculatorTest {
 	@Test
 	public void getTotalTest() {
 		Calculator calc = new Calculator();
-		
-		System.out.print(calc.getTotal());
-		assertEquals("0", outContent.toString());
+		assertTrue(0 == calc.getTotal());
 	}
 	@Test
 	public void addTest() {
 		Calculator calc = new Calculator();
 		calc.add(3);
 		calc.add(7);
-		System.out.print(calc.getTotal());
-		assertEquals("10", outContent.toString());
+		assertTrue(10 == calc.getTotal());
 	}
 	@Test
 	public void subTest() {
 		Calculator calc = new Calculator();
 		calc.subtract(2);
 		calc.subtract(5);
-		System.out.print(calc.getTotal());
-		assertEquals("-7", outContent.toString());
+		assertTrue(-7 == calc.getTotal());
 	}
 	@Test
 	public void multiTest() {
 		Calculator calc = new Calculator();
 		calc.add(2);
 		calc.multiply(2);
-		System.out.print(calc.getTotal());
-		assertEquals("4", outContent.toString());
+		assertTrue(4 == calc.getTotal());
 	}
 	@Test
 	public void divTest() {
 		Calculator calc = new Calculator();
 		calc.add(2);
 		calc.divide(2);
-		System.out.print(calc.getTotal());
-		assertEquals("1", outContent.toString());
+		assertTrue(1 == calc.getTotal());
+	}
+	@Test
+	public void divBy0Test() {
+		Calculator calc = new Calculator();
+		calc.add(2);
+		calc.divide(0);
+		assertTrue(0 == calc.getTotal());
 	}
 	@Test
 	public void getHistoryTest() {
 		Calculator calc = new Calculator();
-		
-		System.out.print(calc.getHistory());
-		assertEquals("", outContent.toString());
+		assertTrue("" == calc.getHistory());
 	}
 
 }
